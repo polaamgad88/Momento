926c46e1cae3c45f52643c09d96eb1786bddb690
Adel Amgad <polaamgad288@gmail.com>
2023-08-26 21:57:23 +0300

    addig video bg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: TheEvent
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/theevent-conference-event-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
